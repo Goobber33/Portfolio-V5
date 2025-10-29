e8d523700506071901a73a399c4895c36d7f1896
kyle parks <kcparks1234@gmail.com>
2025-10-29 10:20:30 -0700

    Added code

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
-import flowbite from 'flowbite-react/plugin/nextjs';
-
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    plugins: [flowbite()],
-  },
 };
 
 export default nextConfig;
